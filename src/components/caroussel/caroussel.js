cf1a844043c7bfa8cc36f0a62ad1ae64f1f48f57
camille <camille-emma.d@outlook.com>
2023-02-02 14:27:27 +0100

    home.scss cover ajout max-width 1200px/text-align center

---
+++ src/components/caroussel/caroussel.js
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+import { useParams } from "react-router-dom";
+import cardItems from "../../assets/logements";
+import Vector from "../../assets/Vector.svg";
+import "./caroussel.css";
+
+export default function Caroussel() {
+  const { id } = useParams();
+  const foundItem = cardItems.find((object) => object.id === id);
+  const pictures = foundItem.pictures;
+
+  const [current, setCurrent] = useState(0);
+
+  /**
+   * Si la diapositive actuelle est la dernière diapositive, passez à la première diapositive, sinon passez à la suivante
+   * faire glisser.
+   */
+  const nextSlide = () => {
+    setCurrent(current === pictures.length - 1 ? 0 : current + 1);
+  };
+
+  /**
+   * Si la diapositive actuelle est la première diapositive, définissez la diapositive actuelle sur la dernière diapositive, sinon
+   * définir la diapositive actuelle sur la diapositive précédente.
+   */
+  const prevSlide = () => {
+    setCurrent(current === 0 ? pictures.length - 1 : current - 1);
+  };
+
+  return (
+    <div className="slider">
+      {/* Affichage des boutons de navigation du curseur */}
+      <button className="vectorPrev">
+        <img src={Vector} onClick={prevSlide} alt="fleche" />
+      </button>
+      <button className="vectorNext">
+        <img src={Vector} onClick={nextSlide} alt="fleche" />
+      </button>
+      {/* Images */}
+      {pictures.map((img, index) => {
+        return (
+          <div key={index}>
+            {index === current && (
+              <img
+                src={img}
+                alt="Photos du logement"
+                className="slider-image"
+              />
+            )}
+            {index === current && (
+              <strong className="image-number">
+                {current + 1}/{pictures.length}
+              </strong>
+            )}
+          </div>
+        );
+      })}
+    </div>
+  );
+}
---
+++ src/components/header/header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../navbar/navbar'
+import LOGO from '../../assets/LOGO.jpg'
+import './header.css'
+
+export default function Header() {
+  return (
+    <div className='container'>
+      <a href="http://localhost:3000/">
+        <img className='logo' src={LOGO} alt="logo"></img></a>
+        <Navbar/>
+    </div>
+  )
+}
